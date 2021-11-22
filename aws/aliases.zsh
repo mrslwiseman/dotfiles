0e64f8cc1f92e4391303d0f9fc4e2b51cc20dda8
Matt Wiseman <matt.wiseman@sine.co>
2021-11-22 22:04:45 +1030

    aws stuff

---
+++ aws/aliases.zsh
@@ -0,0 +1,12 @@
+
+ack-ssm-env() {
+  params=$(aws ssm get-parameters-by-path --with-decryption --recursive --path $1 | jq -r '.Parameters[] | (.Name + " " + .Value)' | sort -u)
+  if [ -z "$2" ]
+    then
+      echo "filtering none"
+      echo $params
+  else
+    echo "filtering $2"
+    echo $params | ack $2
+  fi
+}
