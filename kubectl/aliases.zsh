7da5f97e71c8790de8b26730c51a03fedfaa1307
Matt Wiseman <matt.wiseman@sine.co>
2021-11-22 22:08:09 +1030

    kubes

---
+++ kubectl/aliases.zsh
@@ -0,0 +1,8 @@
+alias k="kubectl"
+alias kall="kubectl get all"
+alias kaf="kubectl apply -f"
+
+alias kgp='kubectl get pod'
+alias kgap="kubectl get pods -o wide --all-namespaces"
+alias kd='kubectl delete pod'
+
