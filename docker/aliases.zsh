91fdfb0b6b3646440031431a58d92d2f74c42ce3
Matt Wiseman <matt.wiseman@sine.co>
2022-06-25 11:43:08 +0930

    add loadEnv function

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,7 @@
 alias dps='docker ps --format "table {{.Names }}\t{{.Image}}\t{{ .State }}\t{{ .Status }}"'
 alias d='docker $*'
 alias d-c='docker-compose $*'
+
+load-env(){
+  export $(cat .env | sed 's/#.*//g' | xargs)
+}
