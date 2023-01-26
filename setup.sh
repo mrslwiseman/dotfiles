553fafb8b9ad29542b0e1bb1d10be473487608a4
Matt Wiseman <mrslwiseman@gmail.com>
2023-01-26 21:43:15 +1030

    tweaks

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
+echo "linking .zimrc file"
+ln -s $HOME/.dotfiles/.zimrc.symlink $HOME/.zimrc
+
+echo "linking .zshrc file"
+ln -s $HOME/.dotfiles/.zshrc.symlink $HOME/.zshrc
+
+echo "linking .gitconfig file"
+ln -s $HOME/.dotfiles/gitconfig.symlink $HOME/.gitconfig
+
 echo "installing zim"
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 
 echo "installing starship"
 curl -sS https://starship.rs/install.sh | sh
-
-echo "linking .zimrc file"
-ln -s $HOME/.dotfiles/zimrc.symlink $HOME/.zimrc
-
-echo "linking .zshrc file"
-ln -s $HOME/.dotfiles/zshrc.symlink $HOME/.zshrc
-
-echo "linking .gitconfig file"
-ln -s $HOME/.dotfiles/gitconfig.symlink $HOME/.gitconfig
