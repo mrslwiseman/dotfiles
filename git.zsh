cbd786657e442fcf90a40960256fd8cc2592c4e5
Matt Wiseman <mrslwiseman@gmail.com>
2023-08-09 11:13:39 +0930

    add git branch cleanup function

--- git.zsh
+++ git.zsh
@@ -2,3 +2,9 @@ alias gs="git status -sb"
 function gb() {
     git checkout $(git branch | fzf)
 }
+
+# prune branches
+function gpb(){
+    git fetch -p
+    git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -d
+}
